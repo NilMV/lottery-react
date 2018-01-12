d0cc3b39c0974333ad1b95b422c1249f1f61255c
NilMV <nil.medvedev17@gmail.com>
2018-01-12 12:24:58 +0300

    add a PickWinner function and finally it works

--- src/App.js
+++ src/App.js
@@ -36,6 +36,17 @@ class App extends Component {
     this.setState({ message: 'You have been entered!' });
   };
 
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' });
+  };
+
   render() {
     return (
       <div>
@@ -58,6 +69,9 @@ class App extends Component {
         </form>
         <hr />
 
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+        <hr/>
         <h1>{this.state.message}</h1>
       </div>
     );
